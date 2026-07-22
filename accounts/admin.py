08059d2665162a992b3b3623692c8fc2ee85cfe5
Svyatoslav2 <51791931+Svyatoslav2@users.noreply.github.com>
2026-07-23 00:36:48 +0300

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,23 +4,23 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    # Поля, которые будут видны при просмотре/редактировании пользователя
+
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'middle_name')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'created_at', 'updated_at', 'deleted_at')}),
     )
-    # Поля, которые будут видны при создании нового пользователя
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'first_name', 'last_name', 'middle_name', 'password1', 'password2'),
         }),
     )
-    # Колонки в общем списке пользователей
+
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
-    # Поля только для чтения (их нельзя изменить вручную)
+
     readonly_fields = ('created_at', 'updated_at', 'deleted_at')
